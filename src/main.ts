4352090920f7d58b078812892ee17d27c88ba8ed
Tim Salikhov <tim.dragonoid@gmail.com>
2026-03-15 19:41:04 +0300

    Added config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Config } from './config/config.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>('APP_PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
