57e34afc654b2ac8b12c8cb1f333d01a5472962a
Tim Salikhov <tim.dragonoid@gmail.com>
2026-03-15 21:43:39 +0300

    Added basic users module

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,24 +1,5 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({ name: 'users' })
-export class User {
-    @PrimaryGeneratedColumn()
-    id!: number;
-
-    @Column({ nullable: false, unique: true })
-    username!: string;
-
-    @Column({ nullable: false })
-    password_hash!: string;
-
-    @Column({ type: 'timestamp' })
-    registered_at!: Date;
-
-    @OneToOne(() => Profile)
-    @JoinColumn()
-    profile!: Profile;
-}
-
 @Entity({ name: 'profiles' })
 export class Profile {
     @PrimaryGeneratedColumn()
@@ -38,4 +19,23 @@ export class Profile {
 
     @Column({ type: 'text', nullable: false })
     medical_record!: string
+}
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: false, unique: true })
+    username!: string;
+
+    @Column({ nullable: false })
+    password_hash!: string;
+
+    @Column({ type: 'timestamp' })
+    registered_at!: Date;
+
+    @OneToOne(() => Profile)
+    @JoinColumn()
+    profile!: Profile;
 }
